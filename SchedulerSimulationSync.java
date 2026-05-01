59850bfbc732bc5670460d72d8e62408890cfc95
hessa99 <445052133@std.psau.edu.sa>
2026-05-01 23:22:19 +0300

    Modify incrementContextSwitch()

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -55,9 +55,14 @@ class SharedResources {
     public static void incrementContextSwitch() {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: Multiple threads might read and write simultaneously!
+        // Protect context switch counter
+            counterLock.lock();
+        try {
         contextSwitchCount++;
+        } finally {
+        counterLock.unlock();
+        }
     }
-
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
